e01a7deaa9bb3b373656b749758fd39331ddbec8
Aman Kumar <amankumar.m99@gmail.com>
2025-09-28 03:25:49 +0530

    updated redirect logic for logged-in user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { authGuard } from './guards/auth/auth.guard';
 import { AccountVerificationComponent } from './components/account-verification/account-verification.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 export const routes: Routes = [
     {
@@ -17,10 +17,10 @@ export const routes: Routes = [
         path: 'verify', component: AccountVerificationComponent
     },
     {
-        path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]
+        path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]
     },
     {
-        path: 'profile', component: ProfileComponent, canActivate: [authGuard]
+        path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]
     },
     {
         path: '', redirectTo: 'login', pathMatch: 'full'
--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,15 +1,39 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
+@Injectable({
+  providedIn: 'root'
+})
 
-  if (authService.isLoggedIn()) {
-    return true;
-  } else {
-    router.navigate(['/login']);
-    return false;
+// export const AuthGuard: CanActivateFn = (route, state) => {
+//   const authService = inject(AuthService);
+//   const router = inject(Router);
+
+//   if (authService.isLoggedIn()) {
+//     return true;
+//   } else {
+//     router.navigate(['/login']);
+//     return false;
+//   }
+// };
+
+
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      // Save the attempted URL for redirecting after login
+      this.authService.redirectUrl = state.url;
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
-};
+}
