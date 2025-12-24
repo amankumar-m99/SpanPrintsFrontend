1d59d75b326e858708b17eb11b2130b4a8896656
Aman Kumar <amankumar.m99@gmail.com>
2025-12-25 02:05:49 +0530

    updated routoinh strategy to hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, Router } from '@angular/router';
+import { provideRouter, Router, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -9,5 +9,5 @@ import { AuthService } from './services/auth/auth.service';
 const authInterceptor: HttpInterceptorFn = (req, next) => new AuthInterceptor(inject(Router), inject(AuthService)).intercept(req, next);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideHttpClient(withInterceptors([authInterceptor]))]
 };
