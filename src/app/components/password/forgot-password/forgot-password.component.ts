ae505ae01dbffa406f09e4d9437d77a1b34c74e1
Aman Kumar <amankumar.m99@gmail.com>
2026-02-20 15:40:43 +0530

    added "back to login"

--- src/app/components/password/forgot-password/forgot-password.component.ts
+++ src/app/components/password/forgot-password/forgot-password.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
