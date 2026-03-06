0e81a45ec3631ae4eb6d210594d1f54001b48c2e
Aman Kumar <amankumar.m99@gmail.com>
2026-03-06 23:55:23 +0530

    added a sample bar chart

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartConfiguration, ChartType, registerables } from 'chart.js';
 
+Chart.register(...registerables);
 @Component({
   selector: 'app-statistics',
   standalone: true,
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.css'
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
 
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartLabels: string[] = ['2022', '2023', '2024'];
+  public barChartType: ChartType = 'bar';
+  public barChartData: ChartConfiguration['data'] = {
+    labels: this.barChartLabels,
+    datasets: [
+      { data: [65, 59, 80], label: 'Series A', backgroundColor: 'red' },
+      { data: [28, 48, 40], label: 'Series B', backgroundColor:'blue' }
+    ]
+  };
+
+  chart:any;
+  public config: any = {
+    type: 'bar',
+    data: this.barChartData
+  }
+
+  ngOnInit(): void {
+    this.chart = new Chart('MyChart', this.config);
+  }
 }
