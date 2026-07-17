135cb122c96f0f2282e0ef7acd0623fc48011f3f
Aman Kumar <amankumar.m99@gmail.com>
2026-07-17 17:18:41 +0530

    added day-relative pipe

---
+++ src/app/pipes/dayRelative/day-relative.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DayRelativePipe } from './day-relative.pipe';
+
+describe('DayRelativePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DayRelativePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
